8a4eeae3359fafff5614f427f03e60216eb6c443
evertondom <jony.everton-@hotmail.com>
2021-12-19 17:47:30 -0300

    Subindo o código

--- src/Conta.java
+++ src/Conta.java
@@ -1,4 +1,35 @@
 
 public class Conta {
 
+	int numero;
+	double limite;
+	double saldo;
+	
+	void depositar (Double valor) {
+		this.saldo += valor;	
+	}
+	
+	Boolean sacar (Double valor) {
+		if (saldo < valor) {
+			return false;
+		}else {
+			this.saldo -= valor;
+			return true;
+		}
+	}
+	
+	
+	Boolean transferir(Conta contaReceber, Double valor) {
+		if (this.saldo < valor) {
+			return false;
+		}else {
+			this.saldo = this.saldo - valor;
+			contaReceber.saldo = contaReceber.saldo + valor;
+			return true;
+		}
+		
+	}
 }
+	
+
+	
--- src/Inicial.java
+++ src/Inicial.java
@@ -2,8 +2,28 @@
 public class Inicial {
 
 	public static void main(String[] args) {
-		System.out.println("Oi");
+		
+		Conta contaA = new Conta();
+		contaA.saldo = 500.00;
+		
+		
+		Conta contaB = new Conta();
+		contaB.saldo = 10.00;
+		
+		contaA.transferir(contaB, 200.00);
+		
+		System.out.println(String.format("Saldo ContaB: %s, Saldo ContaA: %s", contaA.saldo , contaB.saldo));
 
+	}
+	
+	static Boolean transferir(Conta contaEnviar, Conta contaReceber, Double valor){
+		if (contaEnviar.saldo < valor) {
+			return false;
+		}else {
+			contaEnviar.saldo = contaEnviar.saldo - valor;
+			contaReceber.saldo = contaReceber.saldo - valor;
+			return true;
+		}
 	}
 
 }
